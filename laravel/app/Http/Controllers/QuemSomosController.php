a9ccbdc3884b9ba3164ec262ac6ca991e6c790e6
luccaluna18 <luccaluna18@gmail.com>
2025-05-17 13:29:49 -0300

    QuemSomosController Fix: Ajustado o path para o novo dominio

--- laravel/app/Http/Controllers/QuemSomosController.php
+++ laravel/app/Http/Controllers/QuemSomosController.php
@@ -8,7 +8,7 @@ class QuemSomosController extends Controller
 {
     public function index()
 {
-    $carrosselImgs = File::files('/home/u745192978/domains/igrejamosaico.site/public_html/img/carrossel');
+    $carrosselImgs = File::files('/home/u745192978/domains/comunidademosaico.com/public_html/img/carrossel');
 
     $imagens = collect($carrosselImgs)->map(function ($file) {
         return 'img/carrossel/' . $file->getFilename();
